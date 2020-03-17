92f2fcbdb8163ac3b54fd301ae66db67f835ef5b
Shivang Mistry <shivangmistry96@gmail.com>
2020-03-16 19:09:28 -0700

    Video and playlist added

--- 3_Front_End/public/js/index.js
+++ 3_Front_End/public/js/index.js
@@ -1,3 +1,6 @@
+var videos = [];
+var count = 0;
+
 $(() => {
     init();
     setDay();
@@ -10,22 +13,83 @@ function setDay(){
 
     $("#day").text(days[today.getDay()] + ",");
     $("#date").text(months[today.getMonth()] + " " + today.getDate());
-
 }
 
 function init() {
-    var req = new XMLHttpRequest();
+    const req = new XMLHttpRequest();
     req.responseType = 'json';
     req.open('GET', "/videos", true);
 
     req.onload = function () {
         var response = req.response;
         if (response.type === "error") {
-            alert("Erro occured.")
+            alert("Error loading videos.");
         }
         else {
-            console.log(response);
+            videos = response.data
+            count = response.count
+            setPlayArea(response.startIndex);
         }
     };
     req.send();
+}
+
+function setPlayArea(startIndex){
+    $("#videoPlayer").empty();
+    $("#playlistArea").empty();
+
+    const assetIndex = 0;
+    
+    $("#videoPlayer").append(
+        "<video id='currentVideo' controls autoplay>" +
+            "<source src ='" + videos[startIndex].assets[assetIndex].url + "'  type = 'video/mp4' id = 'videoSource' >" +
+            "<p>Your browser doesn't support HTML5 video. Here is a <a href='" + videos[startIndex].assets[assetIndex].url + "'>link to the video</a> instead.</p>" +
+        "</video >")
+    
+    $('#videoTitle').text(videos[startIndex].metadata.title);
+    $('#videoDescription').text(videos[startIndex].metadata.description);
+
+    $('video').on('ended', function () {
+        // not auto playing when clicked
+        if(startIndex+1<count){
+            console.log("Video ended.");
+            setPlayArea(startIndex + 1);
+        }
+    });
+    
+    const start = parseInt(startIndex) + 1;
+    const end = Math.min(start + 4, count); 
+    
+    setPlaylistArea(start, end);
+}
+
+function setPlaylistArea(start, end){
+
+    videos.slice(start, end).forEach((v, index) => {
+        let t = v.thumbnails[0];
+        let d = v.metadata.duration;
+        const duration = (~~(d/60)).toString() + ":" + (d%60).toString();
+
+        $("#playlistArea").append("<div class='playlistItem' videoid=" + (start+index) + ">" + 
+                "<div>" +
+                    "<img class='thumbnail' src="+t.url+" alt='" + v.metadata.title + "' >" +
+                    "<span class='durationDiv'><span class='duration'>" + duration + "</span><span>" + 
+                "</div>" +
+                v.metadata.title +
+            "</div>");
+    });
+
+    if(end<count){
+        $("#playlistArea").append("<button id='loadButton' class='redButton loadMore'>Load More</button>");
+        $("#loadButton").click(function() {
+            $(this).hide();
+            setPlaylistArea(end, count);
+        })
+    }
+
+    $(".playlistItem").click(function (event) {
+        const startIndex = $(this).attr('videoid');
+        console.log(startIndex);
+        setPlayArea(startIndex);
+    })
 }
